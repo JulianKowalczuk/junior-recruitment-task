afc3eee56aa348f31bcb8675903195210fff9698
J <julassspl@gmail.com>
2018-01-11 00:49:47 +0100

    Integrate with gulp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var browserSync = require('browser-sync').create();
+var gulp = require('gulp');
+var gulpCleanCSS = require('gulp-clean-css');
+var gulpRename = require('gulp-rename');
+var gulpSass = require('gulp-sass');
+var gulpUglify = require('gulp-uglify');
+
+gulp.task('sass', function () {
+  return gulp.src('frontend/scss/style.scss')
+    .pipe(gulpSass())
+    .pipe(gulp.dest('frontend/css'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('minify-css', ['sass'], function () {
+  return gulp.src('frontend/css/style.css')
+    .pipe(gulpCleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulpRename({ suffix: '.min' }))
+    .pipe(gulp.dest('frontend/css'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('minify-js', function () {
+  return gulp.src('frontend/js/todo-manager.js')
+    .pipe(gulpUglify())
+    .pipe(gulpRename({ suffix: '.min' }))
+    .pipe(gulp.dest('frontend/js'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('browser-sync', function () {
+  browserSync.init({ server: { baseDir: 'frontend' } });
+});
+
+gulp.task('default', ['sass', 'minify-css', 'minify-js']);
+
+gulp.task('dev', ['browser-sync', 'default'], function () {
+  gulp.watch('frontend/index.html', browserSync.reload);
+  gulp.watch('frontend/scss/style.scss', ['sass']);
+  gulp.watch('frontend/css/style.css', ['minify-css']);
+  gulp.watch('frontend/js/todo-manager.js', ['minify-js']);
+});
